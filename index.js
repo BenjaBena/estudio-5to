53b51fc1d948ab04fb9b9d10827f3400dc98a9de
BenjaBena <111993538+BenjaBena@users.noreply.github.com>
2023-05-04 21:30:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-
+const ejs = require('ejs');
+const path = require('path');
+const fs = require('fs');
 const port = 3000;
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.send('¡Hola Mundo!');
+app.get('/index', (req, res) => {
+  res.send('dale la puta madre');
 });
+app.set('view engine', 'ejs');
 
 app.listen(port, () => {
   console.log(`El servidor está escuchando en el puerto ${port}`);
