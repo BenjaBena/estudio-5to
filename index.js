041ca8c1f306ac3e461fc41331daa064553e201d
BenjaBena <111993538+BenjaBena@users.noreply.github.com>
2023-05-15 10:50:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const path = require('path');
-const fs = require('fs');
+const ejs = require("ejs");
+const path = require("path");
+const fs = require("fs");
 const port = 3000;
-const morgan = require('morgan');
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-app.get('/index', (req, res) => {
-  res.send('dale la puta madre');
+app.get("/", (req, res) => {
+    res.send("si");
 });
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
-  console.log(`El servidor está escuchando en el puerto ${port}`);
+    console.log(`El servidor está escuchando en el puerto ${port}`);
 });
