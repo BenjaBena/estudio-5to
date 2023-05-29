74ac6a0bd9116c68fb8ca1f5885a0a663a49dfe8
BenjaBena <111993538+BenjaBena@users.noreply.github.com>
2023-05-29 10:52:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,36 @@ const port = 3000;
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://JaredPonce:<Fredy0713>@pdbbyjp.fafluhj.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(url, function(err, client) {
+    if (err) {
+    console.log('Error al conectar a la base de datos:', err);
+    } else {
+    console.log('Conexión exitosa a la base de datos');
+
+      // Aquí puedes comenzar a definir tus rutas y configuraciones de Express.js
+      // ...
+
+      // Cierra la conexión de la base de datos cuando sea necesario
+      // client.close();
+    }
+});
+
+app.get('/usuarios', function(req, res) {
+    const db = client.db(); // Obtiene la instancia de la base de datos
+
+    db.collection('usuarios').find({}).toArray(function(err, result) {
+    if (err) {
+        console.log('Error al obtener usuarios:', err);
+        res.status(500).send('Error interno del servidor');
+    } else {
+        res.json(result);
+    }
+    });
+});
+
 // Conexión a la base de datos
 /*mongoose.connect('mongodb://localhost/mydatabase', {
 useNewUrlParser: true,
