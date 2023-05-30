7a63d7a131730f8daf30e8bd06575d124e1423c9
BenjaBena <111993538+BenjaBena@users.noreply.github.com>
2023-05-30 10:07:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,70 +6,65 @@ const fs = require("fs");
 const port = 3000;
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://JaredPonce:<Fredy0713>@pdbbyjp.fafluhj.mongodb.net/?retryWrites=true&w=majority'
-
-MongoClient.connect(url, function(err, client) {
-    if (err) {
-    console.log('Error al conectar a la base de datos:', err);
-    } else {
-    console.log('Conexión exitosa a la base de datos');
-
-      // Aquí puedes comenzar a definir tus rutas y configuraciones de Express.js
-      // ...
-
-      // Cierra la conexión de la base de datos cuando sea necesario
-      // client.close();
-    }
-});
-
-app.get('/usuarios', function(req, res) {
-    const db = client.db(); // Obtiene la instancia de la base de datos
-
-    db.collection('usuarios').find({}).toArray(function(err, result) {
-    if (err) {
-        console.log('Error al obtener usuarios:', err);
-        res.status(500).send('Error interno del servidor');
-    } else {
-        res.json(result);
-    }
-    });
-});
-
-// Conexión a la base de datos
-/*mongoose.connect('mongodb://localhost/mydatabase', {
-useNewUrlParser: true,
-useUnifiedTopology: true
-})
-.then(() => {
-console.log('Conexión exitosa a MongoDB');
-})
-.catch((error) => {
-console.error('Error al conectar a MongoDB:', error);
-});
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
-});
-const User = mongoose.model('User', userSchema);*/
-
-
-
-
-app.get("/index", (req, res) => {
-    res.render('usuario')
-});
-
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+//seteo del las vistas(ASHEI)
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
+mongoose
+    .connect(
+        "mongodb+srv://JaredPonce:Fredy0713@pdbbyjp.fafluhj.mongodb.net/?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log("Conexión exitosa a MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error al conectar a MongoDB", error);
+    });
+
+//Aca vamos a poner los forms(nasheeeeeIIIIIIIIIANASHEIIII)
+const formularioDB = new mongoose.Schema({
+    Nombre: String,
+    Artista: String,
+    Año: Number,
+    Genero: String,
+});
+//Post
+app.post("/formulario", (req, res) => {
+    const { Nombre, Artista, Año, Genero } = req.body;
+
+    const nuevoFormulario = new Formulario({
+        Nombre,
+        Artista,
+        Año,
+        Genero,
+    });
+
+    nuevoFormulario
+        .save()
+        .then(() => {
+            res.send("Formulario enviado y almacenado en MongoDB");
+        })
+        .catch((error) => {
+            res.status(500).send("Error al almacenar el formulario en MongoDB");
+        });
+});
+
+const Formulario = mongoose.model("Formulario", formularioDB);
+app.get("/index", (req, res) => {
+    res.render("usuario");
+});
+
 app.listen(port, () => {
     console.log(`El servidor está escuchando en el puerto ${port}`);
-})
-main().catch(err => console.log(err));
+});
+main().catch((err) => console.log(err));
 
 async function main() {
-await mongoose.connect('mongodb://192.168.40.90/test')}
+    await mongoose.connect("mongodb://192.168.40.90/test");
+}
